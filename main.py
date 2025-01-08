d12a7d50419637d0d469aa6397809081ffd19103
5132713 <5132713@dallasisd.org>
2025-01-08 13:59:57 -0600

    added base files

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import pygame
 import math
 import time
-import random
-from text import draw_text
+from text import drawText
 from fontDict import fonts
 
 pygame.init()
 
 # ---------------- Setting up the screen, assigning some global variables, and loading text fonts
-screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+screen = pygame.display.set_mode((1600, 900), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 fps = 60
-scaleDownFactor = 3
+scaleDownFactor = 5
 screen_width = int(screen.get_width() / scaleDownFactor)
 screen_height = int(screen.get_height() / scaleDownFactor)
 screen_center = [screen_width / 2, screen_height / 2]
@@ -103,7 +102,7 @@ while running:
             string = str(label)
             if items[label] is not None:
                 string = f"{items[label]}: " + string
-            draw_text(screenUI, Endesga.debug_red, montserratRegularAdaptive, 5, screen_height - (30 + 25 * i) / (scaleDownFactor ** (1 / 1.8)), string, Endesga.black, int(3 / scaleDownFactor) + int(3 / scaleDownFactor) < 1, antiAliasing=False)
+            drawText(screenUI, Endesga.debug_red, montserratRegularAdaptive, 5, screen_height - (30 + 25 * i) / (scaleDownFactor ** (1 / 1.8)), string, Endesga.black, int(3 / scaleDownFactor) + int(3 / scaleDownFactor) < 1, antiAliasing=False)
         pygame.mouse.set_visible(False)
         pygame.draw.circle(screenUI, Endesga.black, (mx + 1, my + 1), 2, 1)
         pygame.draw.circle(screenUI, Endesga.white, (mx, my), 2, 1)
